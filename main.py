f43524900e659a4f0c9fff8ee8bc8fa226f4a59b
Nat <natahsp@gmail.com>
2026-04-15 13:47:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,24 @@ VALID_DRUGS = (
     "doxiciclina",
     "enrofloxacina",
     "enrofloxacino",
+    "maropitant",
+    "maropitante",
     "meloxicam",
+    "metronidazol",
+    "omeprazol",
+    "prednisolona"
 )
 while True:
     drug = input("Digite o nome do princípio ativo (sem letras maiúsculas e sem acento): ")
     if drug in VALID_DRUGS:
         break
-    print("Princípio ativo inválido ou ainda indisponível. Digite outro.")
+    print("Princípio ativo inválido ou indisponível. Digite outro.")
 dose = 1.0
 # ask for the weight of the animal and store it in a variable
 weight = float(input("Digite o peso do animal: "))
 # the user types amoxicilina
 if drug == "amoxicilina":
-    dosage = float(input("Qual dosagem desejada entre 10-20mg/kg (digite apenas o número):"))
+    dosage = float(input("Qual a dosagem desejada entre 10-20mg/kg? Digite apenas o número: "))
     dose = dosage * weight
     print(f"Administrar {dose}mg IM/SC/VO a cada 8-12 horas.")
 # the user types dipirona
@@ -30,14 +35,14 @@ elif drug == "dipirona":
 elif drug == "doxiciclina":
     dose = 10 * weight
     print(f"Administrar {dose}mg IV/VO a cada 12-24 horas.")
-# the user types enrofloxacina / enrofloxacino (same cálculo)
+# the user types enrofloxacina / enrofloxacino 
 elif drug in ("enrofloxacina", "enrofloxacino"):
     print("1 - Cão")
     print("2 - Gato")
     while True:
         species = input("Selecione a espécie (digite apenas o número): ")
         if species == "1":
-            dosage = float(input("Qual dosagem desejada entre 5-10mg/kg (digite apenas o número):"))
+            dosage = float(input("Qual dosagem desejada entre 5-10mg/kg? Digite apenas o número: "))
             dose = dosage * weight
             print(f"Administrar {dose}mg IM/VO a cada 12-24 horas.")
             break
@@ -47,6 +52,8 @@ elif drug in ("enrofloxacina", "enrofloxacino"):
             break
         print("Opção inválida. Escolha 1 (Cão) ou 2 (Gato).")
 # the user types maropitant
+if drug in ("maropitant", "maropitante"):
+    print(f"Administrar {weight}mg SC/VO SID")
 # the user types meloxicam
 elif drug == "meloxicam":
     print("1 - Cão")
@@ -62,7 +69,61 @@ elif drug == "meloxicam":
             print(f"Iniciar com {0.1 * weight}mg SC/VO, continuando com {dose}mg VO SID. Pode também ser usada uma dose única de {0.3 * weight}mg.")
             break
         print("Opção inválida. Escolha 1 (Cão) ou 2 (Gato).")
-
 # the user types metronidazol
+if drug == "metronidazol":
+    print("1 - Giardíase")
+    print("2 - Anaeróbicos")
+    while True:
+        case = input("Selecione o caso (digite apenas o número): ")
+        if case == "1":
+            dose = 25 * weight
+            print(f"Administrar {dose}mg BID ou {2*dose}mg SID VO por 5 dias.")
+            break
+        elif case == "2":
+            dose = 15 * weight
+            print(f"Administrar {dose}mg IV/VO BID.")
+            break
+        print("Opção inválida. Escolha 1 ou 2.")
 # the user types omeprazol
+if drug == "omeprazol":
+    dosage = float(input("Qual dosagem desejada entre 0.5-1mg/kg? Digite apenas o número: "))
+    dose = dosage * weight
+    print(f"Administrar {dose}mg VO SID.")
 # the user types prednisolona
+if drug == "prednisolona":
+    print("1 - Cão")
+    print("2 - Gato")
+    species = input("Selecione a espécie (digite apenas o número: ")
+    print("1 - Alergia")
+    print("2 - Imunossupressão")
+    print("3 - Uso prolongado")
+    case = input("Selecione o caso (digite apenas o número): ")
+    while True:
+        if species == "1" and case == "1":
+            dosage = float(input("Qual dosagem desejada entre 0.5-1mg/kg? Digite apenas o número: "))
+            dose = dosage * weight
+            print(f"Administrar {dose}mg IM/VO BID.")
+            break
+        elif species == "1" and case == "2":
+            dose = 2 * weight
+            print(f"Administrar {dose}mg IM/VO BID.")
+            break
+        elif species == "1" and case == "3":
+            dosage = float(input("Qual a dosagem desejada entre 0.5-2mg/kg? Digite apenas o número: "))
+            dose = dosage * weight
+            print(f"Administrar {dose}mg VO em manhãs alternadas.")
+            break
+        elif species == "2" and case == "1":
+            dose = 1 * weight
+            print(f"Administrar {dose}mg IM/VO BID.")
+            break
+        elif species == "2" and case == "2":
+            dose = 3 * weight
+            print(f"Administrar {dose}mg IM/VO BID.")
+            break
+        elif species == "2" and case == "3":
+            dosage = float(input("Qual a dosagem desejada entre 2-4mg/kg? Digite apenas o número: "))
+            dose = dosage * weight
+            print(f"Administrar {dose}mg VO em noites alternadas.")
+            break
+        print("Opção inválida.")
